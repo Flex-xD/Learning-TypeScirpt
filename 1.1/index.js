58ffdbc8a16acb462c232f9c2d7148d83d685c45
Flex-xD <bishtaman88102@gmail.com>
2025-01-20 22:28:41 +0530

    Learned about Literals , Enums , any , unknown and type casting

--- 1.1/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const num = 2;
-console.log(num);
--- Basic-types/index.ts
+++ Basic-types/index.ts
@@ -25,3 +25,64 @@ const coords : [(number | string)[] , (null | boolean)[]] = [
     [1 , 2 , "Good"] , 
     [null , true , false]
 ];
+
+// LITERALS : These are like the instances of the basic variables types
+let directions : "north" | "south" | "east" | "west"; // Representation of Literals
+directions = "south";
+if (directions === "south") {
+    console.log("You have to move east now")
+} else {
+    console.log("It is not in the south direction !")
+}
+
+// Enum (Enumeration)
+enum numbers {
+    small = 100, 
+    medium = 200, 
+    large = 300
+}
+
+enum statusCode {
+    successCode = 200 ,
+    somethingCreated = 201 ,
+    clientError = 400 ,
+    unAuthorized = 401  ,
+    unexpectedError = 500
+}
+
+let value:number;
+value = 200;
+value === numbers.medium ? console.log("Value is" , numbers.medium) : console.log("Value is not" , numbers.medium);
+
+
+// ANY , UNKNOWN and TYPECASTS
+// Assigning any type to a variable will stop the type checking of the typescript 
+let x : any;
+x = 3;
+x = "hello";
+x = true;
+
+const process = (input : any) : void  =>{
+    console.log("processng" , input);
+}
+// This can be string , number , file etc
+process(2); // WORKS
+process("parameter"); // WORKS
+process(true); // WORKS
+
+// UNKNOWN 
+const processing = (input :unknown) : void => {
+    if (typeof input == "number") {
+        console.log(`Processing ${input}`);
+    } else if (typeof input == "string") {
+        console.log(`Processing ${input}`);
+    } else if (input instanceof Blob) {
+        console.log(`Processing ${input}`);
+    }
+}
+
+
+
+processing(1) // Correctly Identified as Number 
+processing("Hello") // Correctly Identified as String 
+processing(new Blob()) // Correctly Identified as Blob 
